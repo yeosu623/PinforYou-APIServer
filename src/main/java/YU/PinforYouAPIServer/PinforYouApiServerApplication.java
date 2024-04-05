a75788f1d67c7fce7f8412429e1ea40c2aadfe73
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-04-05 15:09:34 +0900

    test commit

--- src/main/java/YU/PinforYouAPIServer/PinforYouApiServerApplication.java
+++ src/main/java/YU/PinforYouAPIServer/PinforYouApiServerApplication.java
@@ -8,6 +8,8 @@ public class PinforYouApiServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PinforYouApiServerApplication.class, args);
+
+		// 21911981 정수열
 	}
 
 }
