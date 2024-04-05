63517891b7210e0bdad5da64650bd47b6eec6cb9
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-04-05 15:10:39 +0900

    Update PinforYouApiServerApplication.java

--- src/main/java/YU/PinforYouAPIServer/PinforYouApiServerApplication.java
+++ src/main/java/YU/PinforYouAPIServer/PinforYouApiServerApplication.java
@@ -9,7 +9,7 @@ public class PinforYouApiServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PinforYouApiServerApplication.class, args);
 
-		// 21911981 정수열
+		// 21911981 정수열...
 	}
 
 }
