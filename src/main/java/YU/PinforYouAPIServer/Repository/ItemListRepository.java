f5c4475ce71fdba155200638fbe913d1918e96ae
jiin <wldls836@gmail.com>
2024-07-23 21:16:46 +0900

    barcode, itemList 수정

--- src/main/java/YU/PinforYouAPIServer/Repository/ItemListRepository.java
+++ src/main/java/YU/PinforYouAPIServer/Repository/ItemListRepository.java
@@ -1,6 +1,7 @@
 package YU.PinforYouAPIServer.Repository;
 
 import YU.PinforYouAPIServer.Entity.*;
+import YU.PinforYouAPIServer.Other.BarcodeGenerator;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
@@ -13,15 +14,23 @@ public class ItemListRepository {
     @PersistenceContext
     EntityManager em;
 
-    /*
     public void save(ItemList itemList) {
+        em.persist(itemList);
+    }
+
+    // 바코드 저장
+    public void saveBarcode(ItemList itemList) {
+        // 바코드 생성
+        String barcode = BarcodeGenerator.generateBarcode();
+        itemList.setBarcode(barcode);
+
+        // DB에 최종 저장
         em.persist(itemList);
     }
 
     public ItemList findOne(Long item_list_id) {
         return em.find(ItemList.class, item_list_id);
     }
-    */
 
     public boolean deleteOne(Long item_list_id) {
         ItemList itemList = em.find(ItemList.class, item_list_id);
