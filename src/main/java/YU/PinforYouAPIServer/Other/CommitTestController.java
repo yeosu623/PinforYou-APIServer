17339351647b3eb22be2c443adc530d7a28e5e66
PC010\Administrator <wjdtnduf12@yu.ac.kr>
2024-08-05 08:30:29 +0900

    commit test

---
+++ src/main/java/YU/PinforYouAPIServer/Other/CommitTestController.java
@@ -0,0 +1,39 @@
+package YU.PinforYouAPIServer.Other;
+
+import YU.PinforYouAPIServer.Entity.User;
+import YU.PinforYouAPIServer.Repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class CommitTestController {
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/get1")
+    @ResponseBody
+    public ResponseEntity<String> get1() throws JsonProcessingException {
+        return null;
+    }
+
+    @GetMapping("/get2")
+    @ResponseBody
+    public ResponseEntity<String> get2() throws JsonProcessingException {
+        return null;
+    }
+}
