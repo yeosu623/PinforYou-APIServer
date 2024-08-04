3326380b146e2800a54e04e9918234e322c5b178
PC010\Administrator <wjdtnduf12@yu.ac.kr>
2024-08-05 08:38:30 +0900

    test commit

--- src/main/java/YU/PinforYouAPIServer/Other/CommitTestController.java
+++ src/main/java/YU/PinforYouAPIServer/Other/CommitTestController.java
@@ -36,6 +36,8 @@ public class CommitTestController {
     public ResponseEntity<String> get2() throws JsonProcessingException {
         User user = userRepository.findOne(1L);
 
+        Long age = user.getAge();
+
         return null;
     }
 
